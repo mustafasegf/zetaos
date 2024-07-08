f799b2ad54db2cd74b76ba48b91909c80253376c
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2024-07-08 13:58:07 +0700

    feat: change runner to move EFI to correct folder

--- .cargo/runner.sh
+++ .cargo/runner.sh
@@ -21,26 +21,33 @@ make
 cd -
 
 # Copy the needed files into an ISO image.
-mkdir -p target/iso_root
-cp limine.cfg target/limine/limine{-bios.sys,-bios-cd.bin,-uefi-cd.bin} target/iso_root
 mkdir -p target/iso_root/boot
 cp "$KERNEL" target/iso_root/boot
 
+mkdir -p target/iso_root/boot/limine
+cp limine.cfg target/limine/limine{-bios.sys,-bios-cd.bin,-uefi-cd.bin} target/iso_root/boot/limine
+
+# Create the EFI boot tree and copy Limine's EFI executables over.
+mkdir -p target/iso_root/EFI/BOOT
+cp target/limine/{BOOTX64.EFI,BOOTIA32.EFI} target/iso_root/EFI/BOOT
+
+# Create the bootable ISO.
 xorriso -as mkisofs \
-  -b limine-bios-cd.bin \
+  -b boot/limine/limine-bios-cd.bin \
   -no-emul-boot -boot-load-size 4 -boot-info-table \
-  --efi-boot limine-uefi-cd.bin \
+  --efi-boot boot/limine/limine-uefi-cd.bin \
   -efi-boot-part --efi-boot-image --protective-msdos-label \
   target/iso_root -o "$KERNEL".iso
 
-# For the image to be bootable on BIOS systems, we must run `limine-deploy` on it.
+# Install Limine stage 1 and 2 for legacy BIOS boot.
 target/limine/limine bios-install "$KERNEL".iso
 
 # Run the created image with QEMU.
 qemu-system-x86_64 \
   -machine q35 -cpu qemu64 -M smm=off \
-  -D target/log.txt -d int,guest_errors -no-reboot -no-shutdown \
+  -D target/log.txt -d int,guest_errors \
+  -no-reboot -no-shutdown \
   -serial stdio \
   -boot d \
   -bios "$OVMF" \
-  "$KERNEL".iso
+  -drive file="$KERNEL".iso,format=raw
